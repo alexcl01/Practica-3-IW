0eac69433878aa258598014793bad7b80c2bea05
alexcl01 <acastellanol1@alumnos.nebrija.es>
2022-02-27 13:34:20 +0100

    Practica 3 - IW

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,59 @@
 import React, { FC } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {Container} from "react-bootstrap";
 
-import Contador from './components/Contador';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navigation from './components/Navigation';
+import { Layout } from './components/Layout';
+import { Jumbo } from './components/Jumbotron';
+import {BarNav} from './components/BarNav';
 
 const App:FC = () => {
   return (
-    <div>
-      <Contador/>
-    </div>
+    <React.Fragment >
+       <Jumbo/>
+        <Layout >
+        <Navigation />
+         </Layout>
+     </React.Fragment >
   );
 }
 
 
 export default App;
+
+
+
+/*falta jumbo
+   <BarNav/>
+    <><Jumbo />
+    <div className="layout">
+      <Navigation />
+    </div></>
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
