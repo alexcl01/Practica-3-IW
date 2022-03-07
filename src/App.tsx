c9a5536f36687946efec81c6094b818297080286
alexcl01 <acastellanol1@alumnos.nebrija.es>
2022-03-07 10:24:24 +0100

    Aparece boton al escribir

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import React, { FC } from 'react';
-import logo from './logo.svg';
-import {Container} from "react-bootstrap";
+import Componente from './components/Componente';
 
-import Navigation from './components/Navigation';
-import { Layout } from './components/Layout';
-import { Jumbo } from './components/Jumbotron';
-import {BarNav} from './components/BarNav';
+import Contenedor from './components/Componente'
+
+
 
 const App:FC = () => {
   return (
-    <React.Fragment >
-       <Jumbo/>
-        <Layout >
-        <Navigation />
-         </Layout>
-     </React.Fragment >
+    <div>
+      <Componente/>
+    </div>
   );
 }
 
@@ -22,37 +17,3 @@ const App:FC = () => {
 export default App;
 
 
-
-/*falta jumbo
-   <BarNav/>
-    <><Jumbo />
-    <div className="layout">
-      <Navigation />
-    </div></>
-
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
